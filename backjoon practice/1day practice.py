754c58305241e3d9999a69a3be81a08cea9e44eb
HoneyTasty <ssy15541@gmail.com>
2022-02-13 00:14:00 +0900

    오늘 배운 내용

---
+++ backjoon practice/1day practice.py
@@ -0,0 +1,36 @@
+# a= int(input())
+# x = int 
+# for i in range(a):
+#     A,B=map(int,input().split())
+#     x = 1 + i 
+#     print('Case','#'+str(x) +':',A,'+',B,'=',str(A+B))
+# =======================================================
+# a = int(input())
+# star = 0
+# for i in range(1,a+1):
+#     star = a -1
+#     print(" "*(a-i) + "*"*i)
+# ========================================================
+# a,b = map(int,input().splite())
+# c = list(map(int, input().split()))
+
+# for i in range(a):
+#     if c[i] >= b:
+#         del c[i]
+
+# print(c)
+#=====================================================
+# n,x = map(int,input().split())
+# l = list(map(int,input().split()))
+# a=[]
+# for i in range(n):
+#     if l[i]<x:
+#         a.append(str(l[i]))
+# print(" ".join(a))
+# ====================================================
+# while True:
+#     a,b = map(int,input().split());
+#     if a==0 and b==0:
+#         break
+#     else :
+#         print(a+b)
